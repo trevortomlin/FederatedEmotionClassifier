4cde18fa009a2c99e48937de82c2b5010827b875
Trevor <trevortomlin2@outlook.com>
2023-04-21 14:36:59 -0700

    Added neural network on top of encoder

--- federated_transformer.py
+++ federated_transformer.py
@@ -1,3 +1,4 @@
+import itertools
 from datasets import load_dataset_builder
 from datasets import get_dataset_split_names
 from datasets import load_dataset
@@ -26,24 +27,8 @@ def tokenize(batch):
 
 emotions_encoded = dataset.map(tokenize, batched=True, batch_size=None)
 
-#print(emotions_encoded.column_names)
-
-#model_checkpoint2 = "distilbert-base-uncased"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#model = AutoModel.from_pretrained(model_checkpoint).to(device)
 model = AutoModel.from_pretrained("distilbert-base-uncased-finetuned-emotion/checkpoint-500").to(device)
-
-# text = "I am happy to be here"
-# inputs = tokenizer(text, return_tensors="pt")
-
-# inputs = {k: v.to(device) for k, v in inputs.items()}
-
-# with torch.no_grad():
-#     outputs = model(**inputs)
-
-#print(outputs)
-
-#print(emotions_encoded)
 
 def extract_hidden_states(batch):
     inputs = {k: v.to(device) for k, v in batch.items() if k in tokenizer.model_input_names}
@@ -55,36 +40,87 @@ emotions_encoded.set_format("torch", columns=["input_ids", "attention_mask", "la
 
 emotions_hidden = emotions_encoded.map(extract_hidden_states, batched=True)
 
-X_train = np.array(emotions_hidden["train"]["hidden_state"])
-y_train = np.array(emotions_hidden["train"]["label"])
+X_train = emotions_hidden["train"]["hidden_state"]
+y_train = emotions_hidden["train"]["label"]
 
-X_val = np.array(emotions_hidden["validation"]["hidden_state"])
-y_val = np.array(emotions_hidden["validation"]["label"])
+X_val = emotions_hidden["validation"]["hidden_state"]
+y_val = emotions_hidden["validation"]["label"]
 
-X_test = np.array(emotions_hidden["test"]["hidden_state"])
-y_test = np.array(emotions_hidden["test"]["label"])
+X_test = emotions_hidden["test"]["hidden_state"]
+y_test = emotions_hidden["test"]["label"]
 
 log_reg = LogisticRegression(max_iter=1000)
 log_reg.fit(X_train, y_train)
 score = log_reg.score(X_val, y_val)
-print(score)
+print(f"Logistic Regression: {score}")
 
 
 dummy_clf = DummyClassifier(strategy="most_frequent")
 dummy_clf.fit(X_train, y_train)
 score = dummy_clf.score(X_val, y_val)
-print(score)
+print(f"Dummy Classifier: {score}")
 
-def plot_confusion_matrix(y_preds, y_true, labels):
-    cm = confusion_matrix(y_true, y_preds, normalize="true")
-    fig, ax = plt.subplots(figsize=(6, 6))
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
-    disp.plot(cmap="Blues", ax=ax, colorbar=False, values_format=".2f")
-    plt.title("Normalized Confusion Matrix")
-    plt.show()
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-y_preds = log_reg.predict(X_val)
-plot_confusion_matrix(y_preds, y_val, emotions_encoded["train"].features["label"].names)
+class NeuralNetwork(nn.Module):
+
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.fc1 = nn.Linear(768, 6)
+        
+
+    def forward(self, x):
+        x = self.fc1(x)
+        return x
+
+mymodel = NeuralNetwork()
+
+learning_rate = 0.1
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(mymodel.parameters(), lr=learning_rate)
+
+num_epochs = 2
+for epoch in range(num_epochs):
+    correct = 0
+    for i, (X, y) in enumerate(zip(X_train, y_train)):
+        y_pred = mymodel(X)
+
+        loss = loss_fn(y_pred, y)
+
+        correct += (torch.argmax(y_pred) == y).float().item()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # if (i+1) % 4000 == 0:
+        #     print(f"Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(X_train)}], Loss: {loss.item():.4f}")
+        #     accuracy = correct / (i+1) * 100
+        #     print(f"Accuracy: {accuracy:.2f}")
+
+print("Training Complete")
+
+with torch.no_grad():
+    correct = 0
+    for i, (X, y) in enumerate(zip(X_val, y_val)):
+
+        correct += (torch.argmax(mymodel(X)) == y).float().item()
+
+    accuracy = correct / len(X_val) * 100
+    print(f"Accuracy: {accuracy:.2f}")
+
+# def plot_confusion_matrix(y_preds, y_true, labels):
+#     cm = confusion_matrix(y_true, y_preds, normalize="true")
+#     fig, ax = plt.subplots(figsize=(6, 6))
+#     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+#     disp.plot(cmap="Blues", ax=ax, colorbar=False, values_format=".2f")
+#     plt.title("Normalized Confusion Matrix")
+#     plt.show()
+
+#y_preds = log_reg.predict(X_val)
+#plot_confusion_matrix(y_preds, y_val, emotions_encoded["train"].features["label"].names)
 
 # num_labels = 6
 # model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=num_labels).to(device)
